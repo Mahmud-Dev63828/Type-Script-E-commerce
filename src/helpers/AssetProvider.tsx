1ccc66f318e8ce6f0f9c4fc5f32aab59428a1835
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-09-28 17:14:09 +0600

    icons and image setup completed

---
+++ src/helpers/AssetProvider.tsx
@@ -0,0 +1,58 @@
+import logo from "../assets/homepageAssets/log.png";
+import Mainlogo from "../assets/homepageAssets/Logo.png";
+import bannerOne from "../assets/homepageAssets/bannerSlider.png";
+import bannertwo from "../assets/homepageAssets/bannerSlider2.png";
+import bannerThree from "../assets/homepageAssets/bannerSlider3.png";
+import creditCart from "../assets/homepageAssets/CreditCard.png";
+import haedphone from "../assets/homepageAssets/Headphones.png";
+import pakage from "../assets/homepageAssets/Package.png";
+import Tropy from "../assets/homepageAssets/Trophy.png";
+import categoryOne from "../assets/homepageAssets/categroy1.png";
+import categoryTwo from "../assets/homepageAssets/category2.png";
+import categoryThree from "../assets/homepageAssets/c3.png";
+import categoryfour from "../assets/homepageAssets/c4.png";
+import categoryfive from "../assets/homepageAssets/c5.png";
+import categoriesix from "../assets/homepageAssets/c6.png";
+import featureProductLeft from "../assets/homepageAssets/featuresProductleft.png";
+import mobile from "../assets/homepageAssets/mobile.png";
+import arrival1 from "../assets/homepageAssets/hotDeal.png";
+import arrival2 from "../assets/homepageAssets/hotdeal2.png";
+import FooterLogo from "../assets/homepageAssets/LogoofFooter.png";
+import android from "../assets/homepageAssets/App1.png";
+import apple from "../assets/homepageAssets/App2.png";
+import newsletter from "../assets/homepageAssets/newsletter.png";
+import singleProduct from "../assets/homepageAssets/singleProduc t.png";
+import singleProductContent from "../assets/homepageAssets/SingleProductContent.png";
+
+// type declaration
+type Assets = {
+  [key: string]: string;
+};
+
+export const assets: Assets = {
+  singleProductContent,
+  singleProduct,
+  newsletter,
+  android,
+  apple,
+  headerlogo: logo,
+  FooterLogo,
+  logo: Mainlogo,
+  arrival1,
+  arrival2,
+  bannerOne,
+  bannertwo,
+  bannerThree,
+  creditCart,
+  haedphone,
+  pakage,
+  Tropy,
+  categoryOne,
+  categoryTwo,
+  categoryThree,
+  categoryfour,
+  categoryfive,
+  categoriesix,
+  featureProductLeft,
+  mobile,
+};
---
+++ src/helpers/IconsProvide.tsx
@@ -0,0 +1,57 @@
+import {
+  FaTwitter,
+  FaFacebook,
+  FaYoutube,
+  FaCartShopping,
+  FaUserAstronaut,
+  FaArrowLeft,
+  FaStar,
+  FaRegStarHalfStroke,
+} from "react-icons/fa6";
+import {
+  FaPinterestP,
+  FaReddit,
+  FaInstagramSquare,
+  FaRegHeart,
+  FaChevronDown,
+  FaChevronUp,
+  FaRegStar,
+} from "react-icons/fa";
+import { IoIosSearch } from "react-icons/io";
+import { IoLocationOutline, IoHelpCircleOutline } from "react-icons/io5";
+import { GoGitCompare } from "react-icons/go";
+import { MdOutlineSupportAgent } from "react-icons/md";
+import { BsTelephonePlusFill } from "react-icons/bs";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
+import React from "react";
+
+type Icons = {
+  [key: string]: React.ReactNode,
+};
+
+export const icons: Icons = {
+  cross: <RxCross2 />,
+  emptyStart: <FaRegStar />,
+  fullStar: <FaStar />,
+  halfStar: <FaRegStarHalfStroke />,
+  twitter: <FaTwitter />,
+  facebook: <FaFacebook />,
+  pinterest: <FaPinterestP />,
+  reddit: <FaReddit />,
+  youtube: <FaYoutube />,
+  instagram: <FaInstagramSquare />,
+  cart: <FaCartShopping />,
+  heart: <FaRegHeart />,
+  user: <FaUserAstronaut />,
+  search: <IoIosSearch />,
+  location: <IoLocationOutline />,
+  compare: <GoGitCompare />,
+  support: <MdOutlineSupportAgent />,
+  help: <IoHelpCircleOutline />,
+  call: <BsTelephonePlusFill />,
+  arrow: <MdOutlineArrowRightAlt />,
+  down: <FaChevronDown />,
+  up: <FaChevronUp />,
+  longArrow: <FaArrowLeft />,
+};
