02b709afeb52c03ea5bfe48a04813c273b0e53f7
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-09-30 01:31:51 +0600

    menu added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/HomeComponents/Header/Index";
 import TopNav from "../components/HomeComponents/TopNav/Index";
+import Menu from "../components/HomeComponents/Menu/Index";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <TopNav />
+      <Menu />
     </div>
   );
 };
